0137fce7c20c1f5d81e79b557a993523c04fae0a
yerry102488-web <yerry102488@gmail.com>
2026-06-24 16:46:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ if st.button("🔍 Say Hellow"):
     st.switch_page("pages/1_Say_Hello.py")
 
 if st.button("📊 查看資料"):
-    st.switch_page("pages/2_查看資料.py")
+    st.switch_page("pages/2_Show_Data.py")
 
