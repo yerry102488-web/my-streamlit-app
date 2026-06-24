6470faa4264a5009b69dd324f1a4423c8321be5c
yerry102488-web <yerry102488@gmail.com>
2026-06-24 16:42:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 st.title("🏠 我的 App 首頁")
-st.write("請從左邊選擇功能")
 
-st.image(
-    "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3",
-    width=600
-)
+if st.button("🔍 Say Hellow"):
+    st.switch_page("pages/1_Say_Hello.py")
+
+if st.button("📊 查看資料"):
+    st.switch_page("pages/2_查看資料.py")
+
